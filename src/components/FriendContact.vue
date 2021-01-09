3b5c8f03f250ac24ec133ab2bb1e8ef5c72c2a85
Marlon Fernandez <marlonfsolis@outlook.com>
2021-01-09 09:59:24 -0500

    Allowing that the FriendContact.vue component accept props.

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "FriendContact",
+  props: [
+    'id',
+    'name',
+    'phone',
+    'email'
+  ],
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
